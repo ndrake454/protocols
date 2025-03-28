3a2108b277d192a059023a18486e644d6b392624
ndrake454 <natedrake454@gmail.com>
2025-03-28 09:47:01 -0600

    Delete protocols.firelight.academy/public_html directory

--- protocols.firelight.academy/public_html/admin/includes/admin-footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="admin-footer">
-    <div class="admin-footer-container">
-        <p>&copy; <?= date('Y'); ?> Northern Colorado EMS. All rights reserved.</p>
-        <p>Protocol Admin v1.0</p>
-    </div>
-</footer>
